792f1cb72e93bd6885264ce97665b62c0c3979a5
p23aelliff <p23aelliff@fairfieldprep.org>
2022-11-04 11:24:54 -0400

    lab 6.1 and 6.2

---
+++ Lab_6-1.py
@@ -0,0 +1,11 @@
+#author ate (11/3/22)
+
+#list variable
+my_list = [1, 2, 3, 4, 5]
+list('string')
+
+#print statements 
+print(my_list)
+print(my_list[2])
+print(my_list * 3)
+print(list)
---
+++ Lab_6-2.py
@@ -0,0 +1,13 @@
+#author ate (11/4/22)
+
+#variables
+my_row = ["Preston", "Joe", "unnamed"]
+my_row[2] = "Aidan"
+my_row2 = my_row
+del my_row[1]
+
+#print statements
+print(my_row2)
+print(my_row)
+
+#I have noticed that it took out Joe's name from the list because it was deleted
