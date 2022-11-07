e1e586f6e5c65d6c6947022cb50858a224da529d
p23aelliff <p23aelliff@fairfieldprep.org>
2022-11-07 12:10:56 -0500

    lab 6-3

---
+++ Lab_6-3.py
@@ -0,0 +1,13 @@
+#author ate (11/7/22)
+
+#variables
+colors = ["red", "yellow", "pink", "blue"]
+colors.append("purple")
+colors.extend(["aqua", "navy", "lime"])
+colors.insert(3, "magenta")
+more_colors = colors
+colors.remove("navy")
+
+#print statements
+print(colors)
+print(more_colors)
