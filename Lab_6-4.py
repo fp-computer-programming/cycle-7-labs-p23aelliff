07d0a2caaf0f6ee9b381aca8f3c9c07bc1c475a6
p23aelliff <p23aelliff@fairfieldprep.org>
2022-11-08 13:39:46 -0500

    lab 6-4

---
+++ Lab_6-4.py
@@ -0,0 +1,13 @@
+#author ate (11/8/22)
+
+#variables
+list1 = ["Smyphonic Band", "Environmental Science", "Theology"]
+list1.append("Geometry")
+list1.insert(1, "Environmental Science")
+list1.sort(reverse=False)
+c_list1 = list1.copy()
+c_list1.reverse()
+
+#print statements
+print(list1)
+print(c_list1)
