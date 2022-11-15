45e0c0ec716c949a2f6a96d0043dd52a135d07d3
p23aelliff <p23aelliff@fairfieldprep.org>
2022-11-15 08:42:07 -0500

    lab 5-8

---
+++ Lab_6-5.py
@@ -0,0 +1,18 @@
+#author ate (11/9/22)
+
+#variables and elif statements
+number_list = [2, 4, 6, 8, 10]
+
+#if statement
+if [0, number_list] == 2:
+    print("valid")
+elif [0, number_list] != 2:
+    print("error: list does not meet specifications")
+else:
+    print("error: list does not meet specifications")
+if [4, number_list] ==10:
+    print("valid")
+elif [4, number_list] != 10:
+    print("error: list does not meet specifications")
+else:
+    print("error: list does not meet specifications")
---
+++ Lab_6-7.py
@@ -0,0 +1,12 @@
+#author ate (11/10/22)
+
+#variables
+input1 = 5
+input2 = 20
+input3 = 100000000000
+input_lst = [input1, input2, input3]
+input_double_lst = [input1 * 2, input2 * 2, input3 * 2]
+
+#print statements
+print(input_lst)
+print(input_double_lst)
