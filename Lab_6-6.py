a284d8fc818e02e20a83716c7c860aabf54baf4b
p23aelliff <p23aelliff@fairfieldprep.org>
2022-11-15 08:42:07 -0500

    lab 6.5-8

---
+++ Lab_6-6.py
@@ -0,0 +1,13 @@
+#author ate (11/10/22)
+
+#variables
+word1 = "music"
+word2 = "party"
+word3 = "games"
+word4 = "holidays"
+word5 = "winter"
+list = [word1, word2, word3, word4, word5]
+index_list = [0, 1, 2, 3, 4]
+
+#print statements
+print(index_list)
---
+++ Lab_6-8.py
@@ -0,0 +1,22 @@
+#author ate (11/10/22)
+
+#variables
+num1 = 21
+num2 = 69
+num3 = 420
+num_lst = [num1, num2, num3]
+
+#elif statements
+if num1 == "even":
+    if num2 == "even":
+        if num3 == "even":
+            print("even")
+if num1 == "odd":
+    if num2 == "odd":
+        if num3 == "odd":
+            print("odd")
+else:
+    print("mixed")
+
+#print statements
+print(num_lst)
